6049790c7c2edfd65f170edfc9ba9aaebdad41b3
kayakiya <kayakiya88@gmail.com>
2021-10-27 07:07:59 +0700

    first commit

---
+++ app/Views/themplate/index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Tasty &mdash; </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Free php5 Website Template by freephp5.co" />
+    <meta name="keywords" content="free website templates, free php5, free template, free bootstrap, free website template, php5, css3, mobile first, responsive" />
+    <meta name="author" content="freephp5.co" />
+
+    <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE php5 TEMPLATE 
+	DESIGNED & DEVELOPED by Freephp5.co
+		
+	Website: 		http://freephp5.co/
+	Email: 			info@freephp5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,600i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+
+    <div class="fh5co-loader"></div>
+
+    <div id="page">
+        <nav class="fh5co-nav" role="navigation">
+            <!-- <div class="top-menu"> -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 text-center logo-wrap">
+                        <div id="fh5co-logo"><a href="index.php">CAFFE MONSTERA<span>.</span></a></div>
+                    </div>
+                    <div class="col-xs-12 text-center menu-1 menu-wrap">
+                        <ul>
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="menu.php">Menu</a></li>
+                            <li class="has-dropdown">
+                                <a href="gallery.php">Gallery</a>
+                                <ul class="dropdown">
+                                    <li><a href="#">Events</a></li>
+                                    <li><a href="#">Food</a></li>
+                                    <li><a href="#">Coffees</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="reservation.php">Reservation</a></li>
+                            <li><a href="about.php">About</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <!-- </div> -->
+        </nav>
+
+        <header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" style="background-image: url(images/bg.jpg);" data-stellar-background-ratio="0.5">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="display-t js-fullheight">
+                            <div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+                                <h1>Tempat cafe terbaik yang ada di <em>KOTA BATU</em> </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div id="fh5co-about" class="fh5co-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-pull-4 img-wrap animate-box" data-animate-effect="fadeInLeft">
+                        <img src="images/hero_1.jpeg">
+                    </div>
+                    <div class="col-md-5 col-md-push-1 animate-box">
+                        <div class="section-heading">
+                            <h2>cafe montera</h2>
+                            <p>Cafe ini sangat cocok untuk tempat nongki bareng bersama teman dan keluarga. Selain itu Cafe Monstera juga sangat cocok untuk para remaja hits yang menyukai fotografi karena view di Monstera yang Instagramable baik indoor maupun outdoor.</p>
+                            <p>Jika kalian penikmat view outdoor, di sana disediakan meja dan kursi yang tertata rapi untuk menikmati indahnya pemandangan Kota Batu yang dikelilingi pegunungan.</p>
+                            <p>Sedangkan di dalam ruangan Cafe Monstera didesain dengan desain interior yang modern.Tak hanya itu, disana pengunjung tidak hanya sekedar menikmati menunya saja, wisatawan akan dimanjakan dengan pemandangan alam dari ketinggian.</p>
+                            <p><a href="#" class="btn btn-primary btn-outline">Our History</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="fh5co-featured-menu" class="fh5co-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 fh5co-heading animate-box">
+                        <h2>Menu di coffe Monstera</h2>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Monstera Cafe juga telah mempersiapkan berbagai menu makanan minuman yang harganya tidak bikin kantong kempes</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+                        <div class="fh5co-item">
+                            <img src="images/ab.jpg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by Freephp5.co">
+                            <h3>Fresh Sweet Peach dan Fresh Lava Berry</h3>
+                            <span class="fh5co-price">Rp.20.000-<sup>25.000</sup></span>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+                        <div class="fh5co-item margin_top">
+                            <img src="images/we.jpg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by Freephp5.co">
+                            <h3>Tahu Walik</h3>
+                            <span class="fh5co-price">Rp.15.000<sup></sup></span>
+                        </div>
+                    </div>
+                    <div class="clearfix visible-sm-block visible-xs-block"></div>
+                    <div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+                        <div class="fh5co-item">
+                            <img src="images/ko.jpg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by Freephp5.co">
+                            <h3>Nasi Goreng nanas</h3>
+                            <span class="fh5co-price">Rp.25.000<sup></sup></span>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+                        <div class="fh5co-item margin_top">
+                            <img src="images/lo.jpg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by Freephp5.co">
+                            <h3>Dessert</h3>
+                            <span class="fh5co-price">Rp.20.000<sup></sup></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="fh5co-featured-testimony" class="fh5co-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 fh5co-heading animate-box">
+                        <h2>Testimony</h2>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Pendapat dari pelanggan yang mengkunjungi cofe Monstera</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-5 animate-box img-to-responsive animate-box" data-animate-effect="fadeInLeft">
+                        <img src="images/person_1.jpg" alt="">
+                    </div>
+                    <div class="col-md-7 animate-box" data-animate-effect="fadeInRight">
+                        <blockquote>
+                            <p> &ldquo; Wow tempat Ini sungguh mengagumkan dari fasilitas keindahan yang disajikan dengan makanan enak dan minuman yang menyegarkan. &rdquo;</p>
+                            <p class="author"><cite>&mdash; Jessica</cite></p>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
+            <div class="container">
+                <div class="row row-pb-md">
+                    <div class="col-md-4 fh5co-widget">
+                        <h4>Caffe MONSTERA</h4>
+                        <p>Sangat mudah untuk menyalahkan diri sendiri karena tidak ada masalah. Atau mengkritik dengan ketangguhan. Ketika mereka tiba di dermaga, mereka ditenagai oleh mobil yang luar biasa.</p>
+                    </div>
+                    <div class="col-md-2 col-md-push-1 fh5co-widget">
+                        <h4>Links</h4>
+                        <ul class="fh5co-footer-links">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Menu</a></li>
+                            <li><a href="#">Gallery</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-2 col-md-push-1 fh5co-widget">
+                        <h4>Categories</h4>
+                        <ul class="fh5co-footer-links">
+                            <li><a href="#">Landing Page</a></li>
+                            <li><a href="#">Real Estate</a></li>
+                            <li><a href="#">Personal</a></li>
+                            <li><a href="#">Business</a></li>
+                            <li><a href="#">e-Commerce</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-4 col-md-push-1 fh5co-widget">
+                        <h4>Contact Information</h4>
+                        <ul class="fh5co-footer-links">
+                            <li>198 West 21th Street, <br> Suite 721 New York NY 10016</li>
+                            <li><a href="tel://1234567920">+ 1235 2355 98</a></li>
+                            <li><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
+                            <li><a href="http://https://freephp5.co">freephp5.co</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div class="row copyright">
+                    <div class="col-md-12 text-center">
+                        <p>
+                            <small class="block">&copy; 2016 Free php5. All Rights Reserved.</small>
+                            <small class="block">Designed by <a href="http://freephp5.co/" target="_blank">Freephp5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+                        </p>
+                        <p>
+                            <ul class="fh5co-social-icons">
+                                <li><a href="#"><i class="icon-twitter2"></i></a></li>
+                                <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                                <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                                <li><a href="#"><i class="icon-dribbble2"></i></a></li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
+    </div>
+
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.stellar.min.js"></script>
+    <!-- Flexslider -->
+    <script src="js/jquery.flexslider-min.js"></script>
+    <!-- Main -->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
